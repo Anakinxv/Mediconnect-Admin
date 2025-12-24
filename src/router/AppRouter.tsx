6d0aa72ffb6d8df7ad0ef93100934ae53caae456
Anakinxv <144478975+Anakinxv@users.noreply.github.com>
2025-12-24 16:56:12 -0400

    Big update: new folders, i18n, config tweaks, and a sprinkle of chaos. Surely nothing will break, right? 😅🔧🌍🤞

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Route, Routes as Router, BrowserRouter } from "react-router-dom";
+import { ROUTES } from "./routes";
 
+import Login from "@/features/auth/pages/Login";
 function AppRouter() {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+      <Router>
+        <Route path={ROUTES.LOGIN} element={<Login />} />
+      </Router>
+    </BrowserRouter>
+  );
 }
 
 export default AppRouter;
--- src/stores/useAppStore.tsx
+++ src/stores/useAppStore.tsx
@@ -1,8 +1,6 @@
 import { create } from "zustand";
-import { createLandingSlice } from "./useLandingSlice";
-import { type LandingSlice } from "./useLandingSlice";
+
 import { type GlobalUISlice, createGlobalUISlice } from "./useGlobalUISlice";
-export const useAppStore = create<LandingSlice & GlobalUISlice>((...a) => ({
-  ...createLandingSlice(...a),
+export const useAppStore = create<GlobalUISlice>((...a) => ({
   ...createGlobalUISlice(...a),
 }));
