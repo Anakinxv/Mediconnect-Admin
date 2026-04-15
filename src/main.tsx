c15bc896a326c689b049613a1b09e02759982e46
Anakinxv <144478975+Anakinxv@users.noreply.github.com>
2026-04-15 13:07:44 -0400

    add Vercel Analytics to app root

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "@/i18n/config";
 import MCToast from "./shared/components/MCToast.tsx";
 import { QueryProvider } from "@/config/QueryProvider";
 import MCLoadingSpinner from "@/shared/components/MCLoadingSpinner.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
       <MCLoadingSpinner />
       <App />
       <MCToast />
+      <Analytics />
     </QueryProvider>
   </StrictMode>,
 );
